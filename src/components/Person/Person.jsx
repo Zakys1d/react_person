011f9c7a00660a90832b5c9bf92868409492bc62
Maksym Huk <maksym.h1009@gmail.com>
2026-05-20 20:26:11 +0300

    result

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,19 @@
-// export const Person = ({ person }) => ();
+import React from 'react';
+
+const Person = ({ person }) => {
+  return (
+    <div className="Person">
+      <h2 className="Person__name">{person.name}</h2>
+      {person.age && <p className="Person__age">Age: {person.age}</p>}
+      {person.isMarried ? (
+        <p className="Person__partner">
+          {person.sex === 'm' ? 'Wife' : 'Husband'}: {person.partnerName}
+        </p>
+      ) : (
+        <p className="Person__partner">I am not married</p>
+      )}
+    </div>
+  );
+};
+
+export default Person;
